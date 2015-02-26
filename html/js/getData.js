5c2bcfbd70792e2560cda4c2a504b556b8c4727a
Garvin Guan <garvin.guan@gmail.com>
2015-02-26 16:52:23 -0500

    Starting work on the javascript for populating the html

--- html/js/getData.js
+++ html/js/getData.js
@@ -6,6 +6,21 @@ function wordStat(text) {
     }, {});
 }
 
+function showDefinitions(kanji) {
+    var table = document.querySelector('#word_result');
+    var definitions = document.createElement('tbody');
+    wordtolookup = JSON.stringify(kanji);
+    $.post("/post", wordtolookup,
+	   function(data,status){
+	       kanjis = data[kanji].MatchingKanji;
+	       for (var definition in kanjis) {
+		   keleinfo = kanjis[definition].Keleinfo
+		   releinfo = kanjis[definition].Releinfo
+		   senseinfo = kanjis[definition].Senseinfo
+	       }
+	   });
+}
+
 var input = document.querySelector('#input');
 var button = document.querySelector('#lookupbutton');
 
@@ -13,5 +28,12 @@ button.addEventListener('click', function () {
     statistics = wordStat(input.value);
     for (var word in statistics) {
 	$("#outputarea").append('<input type="submit" value="'+word+'" class="flat-button">');
+    }
+});
+
+input.addEventListener('keyup', function () {
+    statistics = wordStat(input.value);
+    for (var word in statistics) {
+	$("#carousel").append('<div><button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions('+word+')">'+word+': '+statistics[word]+'</button></div>');
     }
 });
