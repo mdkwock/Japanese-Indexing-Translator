3d053b1394ac297044256135c959a7c32b960d95
Garvin Guan <garvin.guan@gmail.com>
2015-02-27 16:21:50 -0500

    Minor adjustments to the css

--- html/js/getData.js
+++ html/js/getData.js
@@ -7,7 +7,6 @@ function wordStat(text) {
 }
 
 function showDefinitions(kanji) {
-    console.log("debugging");
     var table = document.querySelector('#word_result');
     var definitions = document.createElement('tbody');
     console.log("debugging");
@@ -15,22 +14,31 @@ function showDefinitions(kanji) {
     $.post("/post", wordtolookup,
 	   function(data,status){
 	       $("#keleinfo").append(data);
+	       $("#releinfo").append("<h3>Test</h3>");
 	   });
 }
 
 var input = document.querySelector('#input');
 
 input.addEventListener('keyup', function () {
-    document.getElementById("outputarea").innerHTML = "";
-    $(".outputarea").append('<div id="carousel" class="carousel" data-slick="{"slidesToShow": 4, "slidesToScroll": 4}"></div>');
     statistics = wordStat(input.value);
+
+    var numSlidesToShow = 0;
+    for (var key in statistics) {
+	if (statistics.hasOwnProperty(key)) numSlidesToShow++;
+	if (numSlidesToShow > 4) break;
+    }
+
+    document.getElementById("outputarea").innerHTML = "";
+    $(".outputarea").append('<div id="carousel" class="carousel" data-slick="{"slidesToShow": '+numSlidesToShow+', "slidesToScroll": '+numSlidesToShow+'}"></div>');
+
     for (var word in statistics) {
 	$(".carousel").append('<div><button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions(\''+word+'\');">'+word+': '+statistics[word]+'</button></div>');
     }
 
     $('.carousel').slick({
-	slidesToShow: 3,
-	slidesToScroll: 3,
+	slidesToShow: numSlidesToShow,
+	slidesToScroll: numSlidesToShow,
 	dots: true
     });
 });
