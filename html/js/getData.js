aceaa4c83380e87b0d0e89a64aec0317e6198821
Garvin Guan <garvin.guan@gmail.com>
2015-03-11 22:48:45 -0400

    Changed the layout and got rid of the carousel

--- html/js/getData.js
+++ html/js/getData.js
@@ -115,24 +115,10 @@ var input = document.querySelector('#input');
 input.addEventListener('keyup', function () {
     statistics = wordStat(input.value);
 
-    var numSlidesToShow = 0;
-    for (var key in statistics) {
-	if (statistics.hasOwnProperty(key)) numSlidesToShow++;
-	if (numSlidesToShow > 4) break;
-    }
-
     document.getElementById("outputarea").innerHTML = "";
-    $(".outputarea").append('<div id="carousel" class="carousel" data-slick="{"slidesToShow": '+numSlidesToShow+', "slidesToScroll": '+numSlidesToShow+'}"></div>');
-
     for (var word in statistics) {
-	$(".carousel").append('<div><button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions(\''+word+'\');">'+word+': '+statistics[word]+'</button></div>');
+	$(".outputarea").append('<button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions(\''+word+'\');">'+word+' : '+statistics[word]+'</button>');
     }
-
-    $('.carousel').slick({
-	slidesToShow: numSlidesToShow,
-	slidesToScroll: numSlidesToShow,
-	dots: true
-    });
 });
 
 var button = document.querySelector('#lookupkanji');
@@ -145,25 +131,11 @@ button.addEventListener('click', function () {
     $.post("/parse", textToParse,
 	   function(data,status){
 	       results = JSON.parse(data);
-	       var numSlidesToShow = 0;
-	       for (var key in results) {
-		   if (results.hasOwnProperty(key)) numSlidesToShow++;
-		   if (numSlidesToShow > 4) break;
-	       }
-	       console.log(numSlidesToShow);
-	       console.log(results);
 	       document.getElementById("outputarea").innerHTML = "";
-	       $(".outputarea").append('<div id="carousel" class="carousel" data-slick="{"slidesToShow": '+numSlidesToShow+', "slidesToScroll": '+numSlidesToShow+'}"></div>');
 
 	       for (var word in results) {
-		   $(".carousel").append('<div><button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions(\''+word+'\');">'+word+': '+results[word]+'</button></div>');
+		   $(".outputarea").append('<button type="button" value="'+word+'" class="flat-button" onclick="showDefinitions(\''+word+'\');">'+word+' : '+results[word]+'</button>');
 	       }
-
-	       $('.carousel').slick({
-		   slidesToShow: numSlidesToShow,
-		   slidesToScroll: numSlidesToShow,
-		   dots: true
-	       });
 	   });
 
 });
